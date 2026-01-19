179d7f1a254e826aafea5a6d02b1cd5270373b8b
Buriak-byte <oleksii.buriak12@gmail.com>
2026-01-19 18:11:15 +0200

    ddddd

---
+++ switch.js
@@ -0,0 +1,44 @@
+//1
+
+const kindOfDrink = "Сік";
+switch (kindOfDrink) {
+  case "Кава":
+    console.log("мені не можна ще каву");
+  case "Чай":
+    console.log("я не це хочу");
+  case "Сік":
+    console.log("те що треба");
+}
+//2
+
+const weekDay = "Понеділок";
+switch (weekDay) {
+  ´+++++++----
+  // case "Понеділок":
+  //   console.log("це робочий день");
+  // case "Вівторок":
+  //   console.log("це робочий день");
+  // case "Середа":
+  //   console.log("це робочий день");
+  // case "Четвер":
+  //   console.log("це робочий день");
+  // case "П'ятниця":
+  //   console.log("це робочий день");
+  // case "Субота":
+  //   console.log("це вихідний");
+  // case "Неділя":
+  //   console.log("це вихідний");
+}
+
+//3
+// Створіть змінну для зберігання номера місяця. За номером місяця визначайте пору року і виводьте відповідне повідомлення.
+const MonthNummer = 1;
+switch (MonthNummer) {
+  case 1:
+    console.log();
+
+    break;
+
+  default:
+    break;
+}
