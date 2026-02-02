7c9399b4ffc500b6e797cafd8eeb0facfbdea9c5
Buriak-byte <oleksii.buriak12@gmail.com>
2026-02-02 19:24:35 +0200

    ааа

--- switch.js
+++ switch.js
@@ -1,4 +1,4 @@
-//1
+// //1
 
 const kindOfDrink = "Сік";
 switch (kindOfDrink) {
@@ -13,32 +13,71 @@ switch (kindOfDrink) {
 
 const weekDay = "Понеділок";
 switch (weekDay) {
-  ´+++++++----
-  // case "Понеділок":
-  //   console.log("це робочий день");
-  // case "Вівторок":
-  //   console.log("це робочий день");
-  // case "Середа":
-  //   console.log("це робочий день");
-  // case "Четвер":
-  //   console.log("це робочий день");
-  // case "П'ятниця":
-  //   console.log("це робочий день");
-  // case "Субота":
-  //   console.log("це вихідний");
-  // case "Неділя":
-  //   console.log("це вихідний");
+case "Понеділок" || "Вівторок"  || "Середа" || "Четвер" || "П'ятниця":
+  console.log("це робочий день")
+  break
+  case "Субота" || "Неділя":
+  console.log("це вихідний")
+  
 }
 
 //3
 // Створіть змінну для зберігання номера місяця. За номером місяця визначайте пору року і виводьте відповідне повідомлення.
-const MonthNummer = 1;
-switch (MonthNummer) {
-  case 1:
-    console.log();
+const MonthNummer = 7;
+switch (true) {
+  case 12 || 1 || 2:
+    console.log("це зима");
+   
+  case 3 || 4 || 5:
+    console.log("це весна");
+     
+  case 6 || 7 || 8:
+    console.log("це літо");
 
+  case 9 || 10 || 11:
+    console.log("це осінь");
+    
+}
+MonthNummer === 12;
+
+
+//   // 4
+//   // Створіть змінну для зберігання назви кольору. Виводьте повідомлення відповідно до вибраного кольору: якщо "червоний" — "стоп", "зелений" — "йти", "жовтий" — "чекати".
+
+const color = "жовтий"
+switch (color) {
+    case "червоний":
+        console.log("стоп!")
+        break;
+case "зелений":
+    console.log("йти")
+break;
+case "жовтий":
+    console.log("чекати")
+
+
+}
+//5
+// // Створіть змінні для зберігання двох чисел та оператора (як у списку select). Виконайте відповідну операцію та виведіть результат. У випадку ділення на нуль — виведіть попередження.
+const firstNumber = 13
+const secondNumber = 0
+const operator = "/"
+// const result = `${firstNumber}` `${operator}` `${secondNumber}`
+switch (operator) {
+  case "-":
+    console.log(firstNumber - secondNumber);
     break;
-
-  default:
+  case "+":
+    console.log(firstNumber + secondNumber);
+    break;
+  case "*":
+    console.log(firstNumber * secondNumber);
+    break;
+  case "/":
+    console.log(firstNumber / secondNumber);
+    break;
+  case "operator = "/" && secondNumber = 0":
+    
     break;
 }
+console.log(result)
